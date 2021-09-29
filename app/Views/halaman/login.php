1b3c16b73b18ca48fba3dd9689b6e8396c6825d6
fitriindriyani <90949453+fitriindriyani@users.noreply.github.com>
2021-09-29 17:38:42 +0700

    tugas pertemuan 2

--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -7,7 +7,7 @@
         <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
         <p>
             Ini adalah layanan perpustakaan digital masa kini
-            <a href="<?=base_url(relativePath: '/daftar-member')?>">klik daftar</a>
+            <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
         </p>
 
     </body>
