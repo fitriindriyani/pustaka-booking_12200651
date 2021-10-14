09745775e274d8dfdf828b53c95dbaa0aa1ba43a
fitriindriyani <90949453+fitriindriyani@users.noreply.github.com>
2021-10-14 08:27:24 +0700

    tugas pertemuan 4

--- app/Database/Migrations/2021-10-09-063327_PenggunaMigration_12200651.php
+++ app/Database/Migrations/2021-10-09-063327_PenggunaMigration_12200651.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class Pengguna extends Migration
+class PenggunaMigration_12200651 extends Migration
 {
     public function up()
     {
