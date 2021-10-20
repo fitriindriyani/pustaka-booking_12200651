4029c2f8aa2d96baff75fb4ac79847ee191da09d
fitriindriyani <90949453+fitriindriyani@users.noreply.github.com>
2021-10-20 15:48:56 +0700

    tugas pertemuan 5

--- app/Models/Pengguna12200651.php
+++ app/Models/Pengguna12200651.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PenggunaModel_12200651 extends Model
+class Pengguna12200651 extends Model
 {
     protected $DBGroup              = 'koneksiku';
     protected $table                = 'pengguna_12200651';
@@ -13,6 +13,17 @@ class PenggunaModel_12200651 extends Model
     protected $returnType           = 'array';
     protected $protectFields        = true;
     protected $allowedFields        = ['nama', 'password'];
-
-
+    
+/**
+     * method untuk cek login dari table pengguna
+     * berdasarkan nama dan password
+     * @var String $user
+     * @var String pass
+     */
+public function cekLogin($user, $pass){
+    return $this->where('nama', $user)
+                ->where('password', md5($pass) )->first();
+    }
 }
+
+
