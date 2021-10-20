4c18d034e3560d5bf4a123665a2672edae8e834e
fitriindriyani <90949453+fitriindriyani@users.noreply.github.com>
2021-10-20 15:42:56 +0700

    tugas pertemuan 4

--- app/Database/Seeds/PenggunaSeeder12200651.php
+++ app/Database/Seeds/PenggunaSeeder12200651.php
@@ -2,29 +2,29 @@
 
 namespace App\Database\Seeds;
 
-use App\Models\PenggunaModel_12200651;
+use App\Models\Pengguna12200651;
 use CodeIgniter\Database\Seeder;
 
-class PenggunaSeeder_12200651 extends Seeder
+class PenggunaSeeder12200651 extends Seeder
 {
     public function run()
     {
         $data = [
             [
-                'nama'     => 'fitriindriyani',
+                'nama'     => 'fitri',
                 'password' => 'md5'('12200651')
             ],
             [
                 'nama'     => 'admin',
-                'password' => 'md5'('12200651')
+                'password' => 'md5'('12345')
             ],
             [
                 'nama'     => '12200651',
-                'password' => 'md5'('fitriindriyani')
+                'password' => 'md5'('fitri')
             ]          
         ];
 
-        $p = new PenggunaModel_12200651();
+        $p = new Pengguna12200651();
         $p->insertBatch($data);
     }
 }
